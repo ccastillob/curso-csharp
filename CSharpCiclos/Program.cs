c1d14e172aba1b6a9450431a44b8a27066577a31
Christofer Castillo <ccastillo993a@gmail.com>
2023-06-22 19:19:55 -0500

    feat: ciclos

---
+++ CSharpCiclos/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//for(int i=0; i<=5; i++ )
+//{
+//    Console.WriteLine( i );
+//}
+
+//for (int i = 5; i >= 0; i--)
+//{
+//    Console.WriteLine(i);
+//}
+
+int contador = 0;
+
+while (contador <= 5)
+{
+    Console.WriteLine($"El contador es: {contador}");
+    contador++;
+    //contador+= 1;
+    //contador = contador + 1;
+}
+
+foreach (var item in new [] {5,4,3,2,1,0})
+{
+    Console.WriteLine(item);
+}
