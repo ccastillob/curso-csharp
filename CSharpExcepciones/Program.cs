7626b5ef3d1bcd07ac3e9ef24893b9ce1f2580ab
Christofer Castillo <ccastillo993a@gmail.com>
2023-06-22 19:20:47 -0500

    feat: manejo de excepciones

---
+++ CSharpExcepciones/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite un número");
+string numeroString = Console.ReadLine();
+
+try
+{
+    int numero = int.Parse(numeroString);
+    Console.WriteLine($"Has escrito el número: {numero}");
+}
+catch(FormatException fx) 
+{
+    Console.WriteLine("El dato ingresado no es un número");
+
+    #if DEBUG
+        Console.WriteLine(fx.Message);
+        Console.WriteLine(fx.StackTrace);
+    #endif
+}
+catch (Exception)
+{
+    Console.WriteLine("Ha ocurrido un error");
+}
+
+
